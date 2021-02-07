002ba9b8cf398ec87a7e84df50d0864cfe427ab4
Tan Kongpithaksilp <45220855+tangentino@users.noreply.github.com>
2021-02-08 05:44:30 +0700

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,14 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 
 import auth from './auth';
+import todo from './todo';
 
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
   modules: {
     auth,
+    todo,
   },
 });
 
